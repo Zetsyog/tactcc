df2733c3d702f3242078384450e7bc6231f0e56e
Clement R <zetsyog@gmail.com>
2021-01-02 21:13:37 +0100

    test cleanup

--- test/func/test.sh
+++
@@ -1,6 +0,0 @@
-PREFIX="func"
-run_test_simple "test-0/src.scpa" 0
-test_compil_exec "test-1" test_diff
-test_compil_exec "test-2" test_diff
-test_compil_exec "test-3" test_diff
-test_compil_exec_in_file "test-4" test_diff
